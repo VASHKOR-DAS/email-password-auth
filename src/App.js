26b34a8b085699915a5f42c89c5da7bd97e7a761
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-11-06 22:40:11 +0600

    catch on click register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { getAuth } from "firebase/auth";
+import app from './firebase/firebase.init';
+
+const auth = getAuth(app);
+
 function App() {
+
+  const handleRegister = (event) => {
+    // Register button a click korle page refresh/onno page a jay seta jno na jay
+    event.preventDefault()
+
+    //email catch on click register
+    console.log(event.target.email.value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form  onSubmit={handleRegister}>
+        <input type="email" name="email" id="" placeholder='Your Email' />
+        <br />
+        <input type="password" name="password" id="" placeholder='Your Password' />
+        <br />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
