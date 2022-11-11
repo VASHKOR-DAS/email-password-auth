d882bc08c4a8573807ae18302963a0f7e7a67b81
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-11-11 17:27:01 +0600

    loging method apply

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,28 +1,42 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import app from '../../firebase/firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
     const handleSubmit = event => {
         event.preventDefault();
 
         const form = event.target;
-        const email = form.email;
-        const password = form.password;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.error('error', error);
+            })
     }
     return (
         <div>
             <h3 className='text-center text-primary'>Please Login</h3>
-            <Form onClick={handleSubmit} className='w-50 mx-auto'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form onSubmit={handleSubmit} className='w-50 mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicEmail" required>
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control name='email' type="email" placeholder="Enter email" required/>
+                    <Form.Control name='email' type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control name='password' type="password" placeholder="Password" required/>
+                    <Form.Control name='password' type="password" placeholder="Password" required />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -4,11 +4,11 @@ import { Link, Outlet } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            <nav>
-                <Link className='mx-2' to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
-                <Outlet></Outlet>
+            <nav className='text-end'>
+                <Link className='btn btn-sm btn-outline-primary' to='/login'>Login</Link>
+                <Link className='btn btn-sm btn-success mx-2' to='/register'>Register</Link>
             </nav>
+                <Outlet></Outlet>
         </div>
     );
 };
